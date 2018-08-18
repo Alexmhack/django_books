2374c83005ff3c4f237103784fb723f7df483ab5
alexHooper <alexmohack@gmail.com>
2018-08-18 13:12:48 +0530

    remove the fields that are not in csv file

--- book/models.py
+++ book/models.py
@@ -5,15 +5,3 @@ class Book(models.Model):
 	author = models.CharField(max_length=100)
 	year = models.DateField(auto_now_add=True)
 	isbn = models.CharField(max_length=13, help_text='13 character <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
-	review = models.TextField(max_length=100, default='Awesome book to read', help_text='Enter a review for book under 100 words', blank=True)
-
-	RATING = (
-		('1', 'Very Good'),
-		('2', 'Good'),
-		('3', 'Average'),
-		('4', 'Bad'),
-		('5', 'Very Bad'),
-	)
-
-	rating = models.CharField(choices=RATING, max_length=1, default=3, help_text='Rate the book on a scale of 1 to 5')
-	average = models.DecimalField(max_digits=1, decimal_places=1, default=3.0, help_text='Average review score for book')
