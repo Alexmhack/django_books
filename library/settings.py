a7c11f6ddfe1133b292c76123ea08729de5fc5fc
alexHooper <alexmohack@gmail.com>
2018-08-18 10:02:18 +0530

    connect to postgres database from heroku

--- library/settings.py
+++ library/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import json
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -73,10 +74,17 @@ WSGI_APPLICATION = 'library.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+with open('../env.json') as f:
+    DATABASE = json.load(f)
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': DATABASE['NAME'],
+        'USER': DATABASE['USER'],
+        'PASSWORD': DATABASE['PASSWORD'],
+        'HOST': DATABASE['HOST'],
+        'PORT': DATABASE['PORT']
     }
 }
 
