c4ac590e0f235ebe1904d524971ad60acd6ff0ee
alexHooper <alexmohack@gmail.com>
2018-08-18 11:05:00 +0530

    create model for book details

--- book/models.py
+++ book/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+	title = models.CharField(max_length=100)
+	author = models.CharField(max_length=100)
+	year = models.DateField(auto_now_add=True)
+	isbn = models.CharField(max_length=13, help_text='13 character <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
+	review = models.CharField(max_length=100, help_text='Enter a review for book under 100 words', blank=True)
+
+	RATING = (
+		('1', 'Very Good'),
+		('2', 'Good'),
+		('3', 'Average'),
+		('4', 'Bad'),
+		('5', 'Very Bad'),
+	)
+
+	rating = models.CharField(choices=RATING, help_text='Rate the book on a scale of 1 to 5')
+	average = models.DecimalField(help_text='Average review score for book')
