3ba23c2ea924874aebb1f629554621b9bf578963
alexHooper <alexmohack@gmail.com>
2018-08-18 11:11:56 +0530

    register our model in admin site

--- book/admin.py
+++ book/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Book
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+	list_display = ('title', 'author', 'year', 'review')
+	search_fields = ('title', 'author', 'year', 'review', 'average')
