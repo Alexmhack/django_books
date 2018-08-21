72484d8c51f2ffee3424c1700116367f02607200
alexHooper <alexmohack@gmail.com>
2018-08-21 11:15:52 +0530

    detail view should be called with pk or slug

--- book/urls.py
+++ book/urls.py
@@ -9,5 +9,5 @@ from .views import (
 urlpatterns = [
 	path('', IndexView.as_view(), name='index'),
 	path('list/', BookListView.as_view(), name='book-list'),
-	path('<int:id>/detail/', BookDetailView.as_view(), name='book-detail'),
+	path('<int:pk>/detail/', BookDetailView.as_view(), name='book-detail'),
 ]
