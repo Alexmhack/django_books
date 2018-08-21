3bc4cfb1aa75fda1d242125d2507489392c0df73
alexHooper <alexmohack@gmail.com>
2018-08-21 11:05:45 +0530

    create view for index page

--- book/views.py
+++ book/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views import generic
+from django.views import generic, View
 
 from .models import Book
 
@@ -10,3 +10,18 @@ class BookListView(generic.ListView):
 
 class BookDetailView(generic.DetailView):
 	model = Book
+
+
+class IndexView(View):
+	def get(self, request, *args, **kwargs):
+		queryset = Book.objects.all()
+		context = {
+			'total_count': queryset.count(),
+			'book_after_2000': queryset.filter(year__gte=2000).count(),
+			'book_before_2000': queryset.filter(year__lte=2000).count(),
+			'book_before_1990': queryset.filter(year__lte=1990).count(),
+			'book_after_1990': queryset.filter(year__gte=1990).count(),
+			'book_after_1980': queryset.filter(year__gte=1980).count(),
+			'book_before_1980': queryset.filter(year__lte=1980).count(),
+		}
+		return render(request, 'book/index.html', context)
