c9247e8db71bb4edae7c3a18197da1376a71e330
alexHooper <alexmohack@gmail.com>
2018-08-21 21:49:21 +0530

    add view for isbn form

--- api/views.py
+++ api/views.py
@@ -2,7 +2,17 @@ from django.shortcuts import render
 from django.views import View
 
 from book.models import Book
+from .forms import JsonDataForm
 
 class BookAPIView(View):
 	def get(self, request, *args, **kwargs):
 		return render(request, 'api/index.html')
+
+
+class BookAPIFormView(View):
+	def get(self, request, *args, **kwargs):
+		form = JsonDataForm()
+		context = {
+			'form': form
+		}
+		return render(request, 'api/json_form.html', context)
