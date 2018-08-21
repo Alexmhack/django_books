4a078d531ca81c7aa378e55d612bbc53df7a0d61
alexHooper <alexmohack@gmail.com>
2018-08-21 09:06:58 +0530

    create model for book

--- book/models.py
+++ book/models.py
@@ -1,7 +1,16 @@
+import datetime
 from django.db import models
+
+current_year = datetime.datetime.now().year
 
 class Book(models.Model):
 	title = models.CharField(max_length=100)
 	author = models.CharField(max_length=100)
-	year = models.DateField(auto_now_add=True)
-	isbn = models.CharField(max_length=13, help_text='13 character <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
+	year = models.PositiveSmallIntegerField(
+		default=current_year,
+		help_text='Enter the year YYYY when book was published'
+	)
+	isbn = models.CharField(
+		max_length=10,
+		help_text='10 character <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>'
+	)
