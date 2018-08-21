81b654d6574dd620db16f7ca1743c82814ae206b
alexHooper <alexmohack@gmail.com>
2018-08-21 23:14:59 +0530

    add url for json response of book data

--- api/urls.py
+++ api/urls.py
@@ -3,9 +3,11 @@ from django.urls import path
 from .views import (
 	BookAPIView,
 	BookAPIFormView,
+	BookAPIJson,
 )
 
 urlpatterns = [
 	path('', BookAPIView.as_view(), name='book-api'),
 	path('isbn-form/', BookAPIFormView.as_view(), name='isbn-form'),
+	path('<int:isbn>', BookAPIJson.as_view(), name='api-json'),
 ]
