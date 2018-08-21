a305381a7da9bdadb4244213b790f0515b541385
alexHooper <alexmohack@gmail.com>
2018-08-21 09:33:35 +0530

    redirect root url to our book

--- library/urls.py
+++ library/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('book/', include('book.urls')),
+    path('', RedirectView.as_view(url='/book/', permanent=True)),
 ]
