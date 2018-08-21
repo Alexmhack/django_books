83e8967460a6d657119705e6e3ac36a7fe878eec
alexHooper <alexmohack@gmail.com>
2018-08-21 11:06:08 +0530

    add url for index page

--- book/urls.py
+++ book/urls.py
@@ -3,9 +3,11 @@ from django.urls import path
 from .views import (
 	BookListView,
 	BookDetailView,
+	IndexView,
 )
 
 urlpatterns = [
+	path('', IndexView.as_view(), name='index'),
 	path('list/', BookListView.as_view(), name='book-list'),
 	path('<int:id>/detail/', BookDetailView.as_view(), name='book-detail'),
 ]
