045e4fd2240821e5246b2946feaa5bbdd6b388c9
alexHooper <alexmohack@gmail.com>
2018-08-21 21:36:03 +0530

    create view for api docs

--- api/views.py
+++ api/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+from book.models import Book
+
+class BookAPIView(View):
+	def get(self, request, *args, **kwargs):
+		return render(request, 'api/index.html')
