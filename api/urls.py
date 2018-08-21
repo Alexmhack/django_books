1539519195a3155e5d0702d36cddbbde278aa930
alexHooper <alexmohack@gmail.com>
2018-08-21 21:49:01 +0530

    add url for isbn form

--- api/urls.py
+++ api/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 
 from .views import (
 	BookAPIView,
+	BookAPIFormView,
 )
 
 urlpatterns = [
 	path('', BookAPIView.as_view(), name='book-api'),
+	path('isbn-form/', BookAPIFormView.as_view(), name='isbn-form'),
 ]
