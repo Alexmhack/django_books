917a4935057704a38f49a6317b9a717f0d7b119e
alexHooper <alexmohack@gmail.com>
2018-08-21 09:28:21 +0530

    correct error by using .as_view in class-based view

--- book/urls.py
+++ book/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from .views import BookListView
 
 urlpatterns = [
-	path('list/', BookListView, name='book-list'),
+	path('list/', BookListView.as_view(), name='book-list'),
 ]
