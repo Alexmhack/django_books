67ef149d553022c0d7834a5b8bdcfa94931caa8c
alexHooper <alexmohack@gmail.com>
2018-08-21 11:43:53 +0530

    add pagination logic

--- book/views.py
+++ book/views.py
@@ -1,11 +1,27 @@
 from django.shortcuts import render
 from django.views import generic, View
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
 from .models import Book
 
 class BookListView(generic.ListView):
 	model = Book
 	paginate_by = 10
+
+	def get_context_data(self, *args, **kwargs):
+		queryset = super().get_context_data()
+		book_list = Book.objects.all()
+		paginator = Paginator(book_list, 25)
+		page = self.request.GET.get('page')
+		try:
+			book_page = paginator.page(page)
+		except PageNotAnInteger:
+			book_page = paginator.page(1)
+		except EmptyPage:
+			book_page = paginator.page(paginator.num_pages)
+
+		queryset['book_page'] = book_page
+		return queryset
 
 
 class BookDetailView(generic.DetailView):
