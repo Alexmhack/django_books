6eff5a165c5d0067484a2e294071736cc45e6d77
alexHooper <alexmohack@gmail.com>
2018-08-22 10:45:40 +0530

    add url for search results display

--- book/urls.py
+++ book/urls.py
@@ -4,10 +4,12 @@ from .views import (
 	BookListView,
 	BookDetailView,
 	IndexView,
+	SearchFormResultsView,
 )
 
 urlpatterns = [
 	path('', IndexView.as_view(), name='index'),
 	path('list/', BookListView.as_view(), name='book-list'),
 	path('<int:pk>/detail/', BookDetailView.as_view(), name='book-detail'),
+	path('search-results/', SearchFormResultsView.as_view(), name='search-results'),
 ]
